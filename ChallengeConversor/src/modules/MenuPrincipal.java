4a025ebb95f1b2fda4d0d7b58ed78548f2b3cc61
JoseCarlos-Filho <etijcvivi@gmail.com>
2023-04-03 21:34:39 -0300

    refactor: alterando a chamada de classe MenuConversaoDeMoedas para Entrada de valores

--- ChallengeConversor/src/modules/MenuPrincipal.java
+++ ChallengeConversor/src/modules/MenuPrincipal.java
@@ -3,9 +3,6 @@ package modules;
 import javax.swing.*;
 
 public class MenuPrincipal {
-	
-	static Object[] valores = { "real para dolar", "real para libras", "real para peso argentino" };
-	static String inicial = "real para dolar";
 
 
 	public void TelaInicial(Object[] selectionValues, String initialSelection) {
@@ -15,8 +12,8 @@ public class MenuPrincipal {
 		System.out.println(selection);
 		
 		if (selection == "Conversor de Moedas") {
-			MenuConversaoDeMoedas mcm = new MenuConversaoDeMoedas();
-			mcm.TelaInicial(valores, inicial);
+			EntradaDeValores edv = new EntradaDeValores();
+			edv.InputValor();
 		}
 		
 	}
