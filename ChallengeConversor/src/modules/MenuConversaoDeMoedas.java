da734e522849237ad4bc2d567048432145054509
JoseCarlos-Filho <etijcvivi@gmail.com>
2023-04-11 20:50:58 -0300

    feat: adicionando lista de conversões e metodos de referencia da classe ConverteMoedas e MoedasParaReais

--- ChallengeConversor/src/modules/MenuConversaoDeMoedas.java
+++ ChallengeConversor/src/modules/MenuConversaoDeMoedas.java
@@ -5,27 +5,45 @@ import javax.swing.JDialog;
 import javax.swing.JOptionPane;
 
 public class MenuConversaoDeMoedas extends MenuPrincipal {
-	
-	private Object[] valores = { "De Reais a Dolar","De Reais a Euros", "De Reais a Libras", "De Reais a Peso Argentino", "De Reais a Peso Chileno", "De Dólares a Reais", "De Euros a Reais", "De Libras a Reais", "De Peso Argentino a Reais", "De Peso Chileno a Reais" };
+
+	private Object[] valores = { "De Reais a Dolar", "De Reais a Euros", "De Reais a Libras",
+			"De Reais a Peso Argentino", "De Reais a Peso Chileno", "De Dólares a Reais", "De Euros a Reais",
+			"De Libras a Reais", "De Peso Argentino a Reais", "De Peso Chileno a Reais" };
 	private String inicial = "real para dolar";
-	
-	
-	ConverteMoedas cm = new ConverteMoedas();
+
+	ConverteMoedas converteMoedas = new ConverteMoedas();
+	MoedasParaReais converteReais = new MoedasParaReais();
 
 	public void selecionaTipoDeConversao(double valorOk) {
 		JDialog.setDefaultLookAndFeelDecorated(true);
-		Object selection = JOptionPane.showInputDialog(null, "Selecione uma opção", "Conversores",
+		String selection = (String) JOptionPane.showInputDialog(null, "Selecione uma opção", "Conversores",
 				JOptionPane.QUESTION_MESSAGE, null, valores, inicial);
 		System.out.println(selection);
-		//teste de branch
-		for (Object valor : valores) {
-			
-			if(valor == "real para dolar") {
-				cm.converteReaisParaDolar(valorOk);
-			}
+
+		switch (selection) {
+			case "De Reais a Dolar":
+				converteMoedas.converteReaisParaDolar(valorOk);
+				break;
+			case "De Reais a Euros":
+				converteMoedas.converteReaisParaEuro(valorOk);
+				break;
+			case "De Reais a Libras":
+				converteMoedas.converteReaisParaLibras(valorOk);
+				break;
+			case "De Reais a Peso Argentino":
+				converteMoedas.converteReaisParaPesosArgentinos(valorOk);
+				break;
+			case "De Reais a Peso Chileno":
+				converteMoedas.converteReaisParaPesosChilenos(valorOk);
+				break;
+			case "De Dólares a Reais":
+				converteReais.converteDolarParaReais(valorOk);
+				break;
+			case "De Euros a Reais":
+				converteReais.converteEuroParaReais(valorOk);
+				break;
 		}
-		
+
 	}
-	
-	
+
 }
