91d69c2756a595e933ec9b529eb1529389255b5d
JoseCarlos-Filho <etijcvivi@gmail.com>
2023-04-11 20:47:35 -0300

    feat: criando metodos de conversão de moedas.

--- ChallengeConversor/src/modules/ConverteMoedas.java
+++ ChallengeConversor/src/modules/ConverteMoedas.java
@@ -11,4 +11,28 @@ public class ConverteMoedas {
 
 	}
 
+	public void converteReaisParaEuro(double inputValor) {
+		double moedaEuro = inputValor / 10.85;
+        moedaEuro = (double) Math.round(moedaEuro * 100d) / 100;
+        JOptionPane.showMessageDialog(null, "Você tem $ " + moedaEuro + " Euros");
+	}
+
+	public void converteReaisParaLibras(double inputValor) {
+		double moedaLibra = inputValor / 6.33;
+        moedaLibra = (double) Math.round(moedaLibra * 100d) / 100;
+        JOptionPane.showMessageDialog(null, "Você tem $ " + moedaLibra + " Libras Esterlinas");
+	}
+	
+	public void converteReaisParaPesosArgentinos(double inputValor) {
+		double moedaPesoArgentino = inputValor / 0.039;
+        moedaPesoArgentino = (double) Math.round(moedaPesoArgentino * 100d) / 100;
+        JOptionPane.showMessageDialog(null, "Você tem $ " + moedaPesoArgentino + "Pesos Argentinos");
+	}
+
+	public void converteReaisParaPesosChilenos(double inputValor) {
+		double moedaPesoChileno = inputValor / 0.0040;
+        moedaPesoChileno = (double) Math.round(moedaPesoChileno * 100d) / 100;
+        JOptionPane.showMessageDialog(null, "Você tem $ " + moedaPesoChileno + " Pesos Chilenos");
+	}
+
 }
