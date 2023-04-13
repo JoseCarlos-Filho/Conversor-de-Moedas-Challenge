0f3a312053457720160ce68d072c17e8d9640d6f
JoseCarlos-Filho <etijcvivi@gmail.com>
2023-04-12 21:38:00 -0300

    feat: adicionando novas funçoes de conversão de moedas para reais

--- ChallengeConversor/src/modules/MoedasParaReais.java
+++ ChallengeConversor/src/modules/MoedasParaReais.java
@@ -16,4 +16,22 @@ public class MoedasParaReais {
         JOptionPane.showMessageDialog(null, "Você tem $ " + moedaEuro + " Reais");	
 	}
 
+	public void converteLibrasParaReais(double inputValor) {
+		double moedaLibra = inputValor * 6.33;
+        moedaLibra = (double) Math.round(moedaLibra * 100d) / 100;
+        JOptionPane.showMessageDialog(null, "Você tem $ " + moedaLibra + " Reais");
+	}
+
+	public void convertePesoArgentinoParaReais(double inputValor) {
+		double moedaPesoArgentino = inputValor * 0.039;
+        moedaPesoArgentino = (double) Math.round(moedaPesoArgentino * 100d) / 100;
+        JOptionPane.showMessageDialog(null, "Você tem $" + moedaPesoArgentino + " Reais");
+	}
+
+	public void convertePesoChilenoParaReais(double inputValor) {
+		 double moedaPesoChileno = inputValor * 0.0040;
+	        moedaPesoChileno = (double) Math.round(moedaPesoChileno * 100d) / 100;
+	        JOptionPane.showMessageDialog(null, "Você tem $" + moedaPesoChileno + " Reais");
+	}
+
 }
