95a1d2f1182028e2b587663810e89ba2e5b2b85e
JoseCarlos-Filho <etijcvivi@gmail.com>
2023-04-23 21:27:53 -0300

    feat: importando o package util com a classe VerificaInput.

--- ChallengeConversor/src/moedas/EntradaDeValoresMoedas.java
+++ ChallengeConversor/src/moedas/EntradaDeValoresMoedas.java
@@ -1,38 +1,32 @@
-package modules;
+package moedas;
 
 import javax.swing.JOptionPane;
 
 import application.Program;
+import util.VerificaInput;
 
-public class EntradaDeValores extends MenuPrincipal  {
+public class EntradaDeValoresMoedas {
 
 	public void InputValor() {
-		
+
 		MenuConversaoDeMoedas mcm = new MenuConversaoDeMoedas();
+		VerificaInput verificaInput = new VerificaInput();
 
 		String input = JOptionPane.showInputDialog("Digite um valor: ");
 
-		if (verifica(input)) {
+		if (verificaInput.verifica(input)) {
 			double valorOk = Double.parseDouble(input);
 			mcm.selecionaTipoDeConversao(valorOk);
 			int resposta = JOptionPane.showConfirmDialog(null, "Deseja continuar?");
-            if (JOptionPane.OK_OPTION == resposta) {
-                System.out.println("Escolha opção Afirmativa");
-                Program.main(null);
-            } else {
-                JOptionPane.showMessageDialog(null, "Programa finalizado");
-            }
+			if (JOptionPane.OK_OPTION == resposta) {
+				System.out.println("Escolha opção Afirmativa");
+				Program.main(null);
+			} else {
+				JOptionPane.showMessageDialog(null, "Programa finalizado");
+			}
 		} else {
 			JOptionPane.showMessageDialog(null, "Valor inválido");
 		}
 
-	}
-
-	private boolean verifica(String input) {
-		double valor = Double.parseDouble(input);
-		if (valor >= 0 || valor < 0) {
-			return true;
-		}
-		throw new NumberFormatException("Numero não pode ser 0 ou menor que 0");
 	}
 }
